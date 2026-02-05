d0c194a00c8f7fb55c6fd8a4d06a795c15be9373
alvinqid <github@alvinq.my.id>
2026-02-05 17:48:53 +0700

    test

--- xmake.lua
+++ xmake.lua
@@ -4,33 +4,54 @@ add_rules("mode.release")
 
 add_requires("nlohmann_json 3.11.3")
 
-local function common_cfg(t)
-    t:add_files("src/**.cpp")
-    t:add_includedirs("src", {public = true})
-    t:add_defines("PRELOADER_EXPORT", "UNICODE")
-    t:add_packages("nlohmann_json")
+-- =======================
+-- STATIC LIB
+-- =======================
+target("preloader_static")
+    set_kind("static")
+    set_basename("preloader")
+
+    add_files("src/**.cpp")
+    add_includedirs("src", {public = true})
+    add_defines("PRELOADER_EXPORT", "UNICODE")
+    add_packages("nlohmann_json")
 
     if is_plat("android") then
-        t:add_syslinks("log", "android", "EGL", "GLESv3")
+        add_syslinks("log", "android", "EGL", "GLESv3")
 
         if is_arch("armeabi-v7a") then
-            t:add_linkdirs("lib/ARM")
+            add_linkdirs("lib/ARM")
         elseif is_arch("arm64-v8a") then
-            t:add_linkdirs("lib/ARM64")
+            add_linkdirs("lib/ARM64")
         else
             raise("Unsupported ABI")
         end
 
-        t:add_links("GlossHook")
+        add_links("GlossHook")
     end
-end
 
-target("preloader_static")
-    set_kind("static")
-    set_basename("preloader")
-    common_cfg(target)
-
+-- =======================
+-- SHARED LIB
+-- =======================
 target("preloader_shared")
     set_kind("shared")
     set_basename("preloader")
-    common_cfg(target)
+
+    add_files("src/**.cpp")
+    add_includedirs("src", {public = true})
+    add_defines("PRELOADER_EXPORT", "UNICODE")
+    add_packages("nlohmann_json")
+
+    if is_plat("android") then
+        add_syslinks("log", "android", "EGL", "GLESv3")
+
+        if is_arch("armeabi-v7a") then
+            add_linkdirs("lib/ARM")
+        elseif is_arch("arm64-v8a") then
+            add_linkdirs("lib/ARM64")
+        else
+            raise("Unsupported ABI")
+        end
+
+        add_links("GlossHook")
+    end
