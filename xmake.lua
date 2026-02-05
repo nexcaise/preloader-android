41d85daa2c28c5124813ee7d89dbbf9667237598
alvinqid <github@alvinq.my.id>
2026-02-05 17:45:35 +0700

    test

--- xmake.lua
+++ xmake.lua
@@ -4,24 +4,24 @@ add_rules("mode.release")
 
 add_requires("nlohmann_json 3.11.3")
 
-local function common_cfg(target)
-    target:add_files("src/**.cpp")
-    target:add_includedirs("src", {public = true})
-    target:add_defines("PRELOADER_EXPORT", "UNICODE")
-    target:add_packages("nlohmann_json")
+local function common_cfg(t)
+    t:add_files("src/**.cpp")
+    t:add_includedirs("src", {public = true})
+    t:add_defines("PRELOADER_EXPORT", "UNICODE")
+    t:add_packages("nlohmann_json")
 
     if is_plat("android") then
-        target:add_syslinks("log", "android", "EGL", "GLESv3")
+        t:add_syslinks("log", "android", "EGL", "GLESv3")
 
         if is_arch("armeabi-v7a") then
-            target:add_linkdirs("lib/ARM")
+            t:add_linkdirs("lib/ARM")
         elseif is_arch("arm64-v8a") then
-            target:add_linkdirs("lib/ARM64")
+            t:add_linkdirs("lib/ARM64")
         else
             raise("Unsupported ABI")
         end
 
-        target:add_links("GlossHook")
+        t:add_links("GlossHook")
     end
 end
 
